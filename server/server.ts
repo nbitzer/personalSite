43dad819e03761961408d050f30d8c4487dbc194
Nick Bitzer <nbitzer@gmail.com>
2022-05-21 17:34:00 -0600

    added basic circleci config, eslint and started moving to typescript

---
+++ server/server.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors = require('cors')
+
+const app = express()
+
+// Set cors for local dev. Need a swtich statement for prod
+app.use(cors())
+
+// Define basic api route to verify server is running
+app.get("/api", (req, resp) => {
+    resp.json({ "testing": ["working", "not wokring"] })
+})
+
+// Start server on port 5000
+app.listen(5000, () => {console.log("Server started on port 5000...")})
